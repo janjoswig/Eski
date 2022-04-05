a33c1a307b9f718f3beabaa671525e8d4e283694
Jan-Oliver Joswig <jan.joswig@fu-berlin.de>
2022-04-05 11:44:42 +0200

    Add global constants

--- eski/primitive_types.pxd
+++ eski/primitive_types.pxd
@@ -5,3 +5,10 @@ ctypedef np.intp_t AINDEX
 ctypedef np.float64_t AVALUE
 ctypedef np.float32_t AVALUE32
 ctypedef np.uint8_t ABOOL
+
+cdef struct Constants:
+    AVALUE kB
+    AVALUE R
+    AVALUE u
+
+cpdef Constants make_constants()
--- eski/primitive_types.pyx
+++ eski/primitive_types.pyx
@@ -5,3 +5,12 @@ P_AVALUE = np.float64
 P_AVALUE32 = np.float32
 P_AINDEX = np.intp
 P_ABOOL = np.uint8
+
+cpdef Constants make_constants():
+    cdef Constants constants = Constants(
+        1.380649e-23,       # kB in J / K
+        8.31446261815324,   # R in J / (K mol)
+        1.66053906660e-27,  # u in kg
+    )
+
+    return constants
